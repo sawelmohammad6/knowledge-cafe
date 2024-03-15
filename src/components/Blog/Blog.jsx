bc73a3be2f3a7293ced1ba42a686fe415009dfa0
Mohammad Sawel <sawelmohammad6@gmail.com>
2024-03-16 01:11:27 +0600

    added all files and code

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,32 +1,39 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
-    const {title, cover,author, author_img, posted_date, reading_time, hashtags} = blog;
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
     return (
-        <div>
-            <img src={cover} alt={`Cover Picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8' src={cover} alt={`Cover Picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
-<img className='w-14' src={author_img} alt="" />
-<div className='ml-6'>
-    <h3 className='text-2xl'>{author}</h3>
-    <p>{posted_date}</p>
-</div>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
                 </div>
                 <div>
-<span>{reading_time} min read</span>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 text-2xl text-red-600'>
+                        <FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
             <p>
                 {
-                hashtags.map((hash,idx) => <span key={idx}> <a href="">#{hash}</a> </span>)
+                    hashtags.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a> </span>)
                 }
             </p>
+            <button onClick={() => handleMarkAsRead(reading_time)}
+            className='text-purple-800 font-bold underline'>Mark as Read</button>
         </div>
     );
 };
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 }
 export default Blog;
